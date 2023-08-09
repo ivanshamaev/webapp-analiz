47c946fd31a6a76f5dae26c49553f000fe95f7b9
Ivan Shamaev <qlik@ivan-shamaev.ru>
2023-08-09 14:21:36 +0000

    some changes in UI

--- frontend/src/components/AnalizForm.js
+++ frontend/src/components/AnalizForm.js
@@ -10,238 +10,156 @@ function AnalizForm() {
   return (
     <div className="container">
       <Form>
-        <Form.Group controlId="dob">
-          <Form.Label>
-            Дата рождения
-            <OverlayTrigger placement="right" overlay={renderTooltip('Введите дату рождения')}>
-              <i className="bi bi-info-circle"></i>
-            </OverlayTrigger>
-          </Form.Label>
-          <Form.Control type="date" />
-        </Form.Group>
+        <div className="form-container">
+          <h3>Параметры анализа</h3>
+          <div className="parameter-group">
+            <span className="parameter-label">Дата рождения</span>
+            <input id="dob" className="parameter-input" type="date" />
+            <span className="parameter-unit"></span>
+          </div>
+          <div className="parameter-group">
+            <span className="parameter-label">Пол</span>
+            <select className="parameter-input" id="gender">
+              <option value="">Мужской</option>
+              <option value="dog">Женский</option>
+            </select>
+            <span className="parameter-unit"></span>
+          </div>
+          <div className="parameter-group">
+            <span className="parameter-label">Показатель 3:</span>
+            <input id="analysisDate" className="parameter-input" type="date" />
+            <span className="parameter-unit"></span>
+          </div>
+        </div>
 
-        <Form.Group controlId="gender">
-          <Form.Label>
-            Пол
-            <OverlayTrigger placement="right" overlay={renderTooltip('Выберите пол')}>
-              <i className="bi bi-info-circle"></i>
-            </OverlayTrigger>
-          </Form.Label>
-          <Form.Control as="select">
-            <option>Мужской</option>
-            <option>Женский</option>
-          </Form.Control>
-        </Form.Group>
+        <hr/>
 
-        <Form.Group controlId="analysisDate">
-          <Form.Label>
-            Дата сдачи анализов
-            <OverlayTrigger placement="right" overlay={renderTooltip('Введите дату сдачи анализов')}>
-              <i className="bi bi-info-circle"></i>
-            </OverlayTrigger>
-          </Form.Label>
-          <Form.Control type="date" />
-        </Form.Group>
+        <div className="form-container">
+          <h3>Значения показателей</h3>
+          <div className="parameter-group">
+            <span className="parameter-label">Гематокрит</span>
+            <input id="hematocrit" className="parameter-input" type="number" step="0.1" />
+            <span className="parameter-unit">%</span>
+          </div>
 
-        <Form.Group controlId="hematocrit">
-          <Form.Label>
-            Гематокрит Value %
-            <OverlayTrigger placement="right" overlay={renderTooltip('Введите значение Гематокрита')}>
-              <i className="bi bi-info-circle"></i>
-            </OverlayTrigger>
-          </Form.Label>
-          <Form.Control type="number" step="0.1" />
-        </Form.Group>
+          <div className="parameter-group">
+            <span className="parameter-label">Гемоглобин</span>
+            <input id="hemoglobin" className="parameter-input" type="number" step="0.1" />
+            <span className="parameter-unit">г/дл</span>
+          </div>
 
-        <Form.Group controlId="hemoglobin">
-          <Form.Label>
-            Гемоглобин Value г/дл
-            <OverlayTrigger placement="right" overlay={renderTooltip('Введите значение Гемоглобина')}>
-              <i className="bi bi-info-circle"></i>
-            </OverlayTrigger>
-          </Form.Label>
-          <Form.Control type="number" step="0.1" />
-        </Form.Group>
+          <div className="parameter-group">
+            <span className="parameter-label">Эритроциты</span>
+            <input id="redBloodCells" className="parameter-input" type="number" step="0.1" />
+            <span className="parameter-unit">млн/мкл</span>
+          </div>
 
-        <Form.Group controlId="redBloodCells">
-          <Form.Label>
-            Эритроциты Value млн/мкл
-            <OverlayTrigger placement="right" overlay={renderTooltip('Введите значение Эритроцитов')}>
-              <i className="bi bi-info-circle"></i>
-            </OverlayTrigger>
-          </Form.Label>
-          <Form.Control type="number" step="0.1" />
-        </Form.Group>
+          <div className="parameter-group">
+            <span className="parameter-label">MCV (ср. объем эритр.)</span>
+            <input id="mcv" className="parameter-input" type="number" step="0.1" />
+            <span className="parameter-unit">фл</span>
+          </div>
 
-        <Form.Group controlId="mcv">
-          <Form.Label>
-            MCV (ср. объем эритр.) Value фл
-            <OverlayTrigger placement="right" overlay={renderTooltip('Введите значение MCV')}>
-              <i className="bi bi-info-circle"></i>
-            </OverlayTrigger>
-          </Form.Label>
-          <Form.Control type="number" step="0.1" />
-        </Form.Group>
+          <div className="parameter-group">
+            <span className="parameter-label">RDW (шир. распред. эритр)</span>
+            <input id="rdw" className="parameter-input" type="number" step="0.1" />
+            <span className="parameter-unit">%</span>
+          </div>
 
-        <Form.Group controlId="rdw">
-          <Form.Label>
-            RDW (шир. распред. эритр) Value %
-            <OverlayTrigger placement="right" overlay={renderTooltip('Введите значение RDW')}>
-              <i className="bi bi-info-circle"></i>
-            </OverlayTrigger>
-          </Form.Label>
-          <Form.Control type="number" step="0.1" />
-        </Form.Group>
+          <div className="parameter-group">
+            <span className="parameter-label">MCH (ср. содер. Hb в эр.)</span>
+            <input id="mch" className="parameter-input" type="number" step="0.1" />
+            <span className="parameter-unit">пг</span>
+          </div>
 
-        <Form.Group controlId="mch">
-          <Form.Label>
-            MCH (ср. содер. Hb в эр.) Value пг
-            <OverlayTrigger placement="right" overlay={renderTooltip('Введите значение MCH')}>
-              <i className="bi bi-info-circle"></i>
-            </OverlayTrigger>
-          </Form.Label>
-          <Form.Control type="number" step="0.1" />
-        </Form.Group>
+          <div className="parameter-group">
+            <span className="parameter-label">МСHС (ср. конц. Hb в эр.)</span>
+            <input id="mchc" className="parameter-input" type="number" step="0.1" />
+            <span className="parameter-unit">%</span>
+          </div>
 
-        <Form.Group controlId="mchc">
-          <Form.Label>
-            МСHС (ср. конц. Hb в эр.) Value г/дл
-            <OverlayTrigger placement="right" overlay={renderTooltip('Введите значение МСHС')}>
-              <i className="bi bi-info-circle"></i>
-            </OverlayTrigger>
-          </Form.Label>
-          <Form.Control type="number" step="0.1" />
-        </Form.Group>
+          <div className="parameter-group">
+            <span className="parameter-label">Тромбоциты</span>
+            <input id="platelets" className="parameter-input" type="number" step="0.1" />
+            <span className="parameter-unit">тыс/мкл</span>
+          </div>
 
-        <Form.Group controlId="platelets">
-          <Form.Label>
-            Тромбоциты Value тыс/мкл
-            <OverlayTrigger placement="right" overlay={renderTooltip('Введите значение Тромбоцитов')}>
-              <i className="bi bi-info-circle"></i>
-            </OverlayTrigger>
-          </Form.Label>
-          <Form.Control type="number" />
-        </Form.Group>
 
-        <Form.Group controlId="leukocytes">
-          <Form.Label>
-            Лейкоциты Value тыс/мкл
-            <OverlayTrigger placement="right" overlay={renderTooltip('Введите значение Лейкоцитов')}>
-              <i className="bi bi-info-circle"></i>
-            </OverlayTrigger>
-          </Form.Label>
-          <Form.Control type="number" />
-        </Form.Group>
 
-        <Form.Group controlId="neutrophilsPercent">
-          <Form.Label>
-            Нейтрофилы (общ.число), % Value %
-            <OverlayTrigger placement="right" overlay={renderTooltip('Введите значение Нейтрофилов')}>
-              <i className="bi bi-info-circle"></i>
-            </OverlayTrigger>
-          </Form.Label>
-          <Form.Control type="number" step="0.1" />
-        </Form.Group>
 
-        <Form.Group controlId="lymphocytesPercent">
-          <Form.Label>
-            Лимфоциты, % Value %
-            <OverlayTrigger placement="right" overlay={renderTooltip('Введите значение Лимфоцитов')}>
-              <i className="bi bi-info-circle"></i>
-            </OverlayTrigger>
-          </Form.Label>
-          <Form.Control type="number" step="0.1" />
-        </Form.Group>
+          <div className="parameter-group">
+            <span className="parameter-label">Лейкоциты</span>
+            <input id="leukocytes" className="parameter-input" type="number" step="0.1" />
+            <span className="parameter-unit">тыс/мкл</span>
+          </div>
 
-        <Form.Group controlId="monocytesPercent">
-          <Form.Label>
-            Моноциты, % Value %
-            <OverlayTrigger placement="right" overlay={renderTooltip('Введите значение Моноцитов')}>
-              <i className="bi bi-info-circle"></i>
-            </OverlayTrigger>
-          </Form.Label>
-          <Form.Control type="number" step="0.1" />
-        </Form.Group>
+          <div className="parameter-group">
+            <span className="parameter-label">Нейтрофилы (общ.число)</span>
+            <input id="neutrophilsPercent" className="parameter-input" type="number" step="0.1" />
+            <span className="parameter-unit">%</span>
+          </div>
 
-        <Form.Group controlId="eosinophilsPercent">
-          <Form.Label>
-            Эозинофилы, % Value %
-            <OverlayTrigger placement="right" overlay={renderTooltip('Введите значение Эозинофилов')}>
-              <i className="bi bi-info-circle"></i>
-            </OverlayTrigger>
-          </Form.Label>
-          <Form.Control type="number" step="0.1" />
-        </Form.Group>
+          <div className="parameter-group">
+            <span className="parameter-label">Лимфоциты</span>
+            <input id="lymphocytesPercent" className="parameter-input" type="number" step="0.1" />
+            <span className="parameter-unit">%</span>
+          </div>
 
-        <Form.Group controlId="basophilsPercent">
-          <Form.Label>
-            Базофилы, % Value %
-            <OverlayTrigger placement="right" overlay={renderTooltip('Введите значение Базофилов')}>
-              <i className="bi bi-info-circle"></i>
-            </OverlayTrigger>
-          </Form.Label>
-          <Form.Control type="number" step="0.1" />
-        </Form.Group>
+          <div className="parameter-group">
+            <span className="parameter-label">Моноциты</span>
+            <input id="monocytesPercent" className="parameter-input" type="number" step="0.1" />
+            <span className="parameter-unit">%</span>
+          </div>
 
-        <Form.Group controlId="neutrophilsAbsolute">
-          <Form.Label>
-            Нейтрофилы, абс. Value тыс/мкл
-            <OverlayTrigger placement="right" overlay={renderTooltip('Введите значение Абсолютных Нейтрофилов')}>
-              <i className="bi bi-info-circle"></i>
-            </OverlayTrigger>
-          </Form.Label>
-          <Form.Control type="number" />
-        </Form.Group>
+          <div className="parameter-group">
+            <span className="parameter-label">Эозинофилы</span>
+            <input id="eosinophilsPercent" className="parameter-input" type="number" step="0.1" />
+            <span className="parameter-unit">%</span>
+          </div>
 
-        <Form.Group controlId="lymphocytesAbsolute">
-          <Form.Label>
-            Лимфоциты, абс. Value тыс/мкл
-            <OverlayTrigger placement="right" overlay={renderTooltip('Введите значение Абсолютных Лимфоцитов')}>
-              <i className="bi bi-info-circle"></i>
-            </OverlayTrigger>
-          </Form.Label>
-          <Form.Control type="number" />
-        </Form.Group>
+          <div className="parameter-group">
+            <span className="parameter-label">Базофилы</span>
+            <input id="basophilsPercent" className="parameter-input" type="number" step="0.1" />
+            <span className="parameter-unit">%</span>
+          </div>
 
-        <Form.Group controlId="monocytesAbsolute">
-          <Form.Label>
-            Моноциты, абс. Value тыс/мкл
-            <OverlayTrigger placement="right" overlay={renderTooltip('Введите значение Абсолютных Моноцитов')}>
-              <i className="bi bi-info-circle"></i>
-            </OverlayTrigger>
-          </Form.Label>
-          <Form.Control type="number" />
-        </Form.Group>
+          <div className="parameter-group">
+            <span className="parameter-label">Нейтрофилы, абс.</span>
+            <input id="neutrophilsAbsolute" className="parameter-input" type="number" step="0.1" />
+            <span className="parameter-unit">тыс/мкл</span>
+          </div>
 
-        <Form.Group controlId="eosinophilsAbsolute">
-          <Form.Label>
-            Эозинофилы, абс. Value тыс/мкл
-            <OverlayTrigger placement="right" overlay={renderTooltip('Введите значение Абсолютных Эозинофилов')}>
-              <i className="bi bi-info-circle"></i>
-            </OverlayTrigger>
-          </Form.Label>
-          <Form.Control type="number" />
-        </Form.Group>
+          <div className="parameter-group">
+            <span className="parameter-label">Лимфоциты, абс.</span>
+            <input id="lymphocytesAbsolute" className="parameter-input" type="number" step="0.1" />
+            <span className="parameter-unit">тыс/мкл</span>
+          </div>
 
-        <Form.Group controlId="basophilsAbsolute">
-          <Form.Label>
-            Базофилы, абс. Value тыс/мкл
-            <OverlayTrigger placement="right" overlay={renderTooltip('Введите значение Абсолютных Базофилов')}>
-              <i className="bi bi-info-circle"></i>
-            </OverlayTrigger>
-          </Form.Label>
-          <Form.Control type="number" />
-        </Form.Group>
+          <div className="parameter-group">
+            <span className="parameter-label">Моноциты, абс.</span>
+            <input id="monocytesAbsolute" className="parameter-input" type="number" step="0.1" />
+            <span className="parameter-unit">тыс/мкл</span>
+          </div>
 
-        <Form.Group controlId="esr">
-          <Form.Label>
-            СОЭ Value мм/ч
-            <OverlayTrigger placement="right" overlay={renderTooltip('Введите значение СОЭ')}>
-              <i className="bi bi-info-circle"></i>
-            </OverlayTrigger>
-          </Form.Label>
-          <Form.Control type="number" />
-        </Form.Group>
+          <div className="parameter-group">
+            <span className="parameter-label">Эозинофилы, абс.</span>
+            <input id="eosinophilsAbsolute" className="parameter-input" type="number" step="0.1" />
+            <span className="parameter-unit">тыс/мкл</span>
+          </div>
+
+          <div className="parameter-group">
+            <span className="parameter-label">Базофилы, абс.</span>
+            <input id="basophilsAbsolute" className="parameter-input" type="number" step="0.1" />
+            <span className="parameter-unit">тыс/мкл</span>
+          </div>
+
+          <div className="parameter-group">
+            <span className="parameter-label">СОЭ</span>
+            <input id="esr" className="parameter-input" type="number" step="0.1" />
+            <span className="parameter-unit">мм/ч</span>
+          </div>
+
+        </div>
 
         <button type="submit" className="btn btn-primary">Получить расшифровку показателей</button>
 
