f8ba0fa3052f9f391ff286544f21cec4bce6680c
Ivan Shamaev <qlik@ivan-shamaev.ru>
2023-08-10 22:36:16 +0000

    some improvements in UI

--- frontend/src/components/AnalizForm.js
+++ frontend/src/components/AnalizForm.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import { OverlayTrigger, Tooltip, Form } from 'react-bootstrap';
 
+import MetricInput from './MetricInput';
+
 function AnalizForm() {
     
   const [isChecked, setIsChecked] = useState(false);
@@ -10,49 +12,7 @@ function AnalizForm() {
   
   const handleCheckboxChange = (event) => {
     setIsChecked(!isChecked);
-    //для жалоб
-    const value = event.target.value;
-    if (selectedCheckboxes.includes(value)) {
-      setSelectedCheckboxes(selectedCheckboxes.filter(item => item !== value));
-    } else {
-      setSelectedCheckboxes([...selectedCheckboxes, value]);
-    }
   };
-
-  // Жалобы Start ----------------------------------------
-
-  const checkboxes = [
-    'Головная боль',
-    'Слабость',
-    'Бессоница',
-    'Тошнота',
-    'Головокружение',
-    'Другое (мы учтем в доработке алгоритма)',
-  ];
-
-  const columnCount = 3;
-  const checkboxesPerColumn = Math.ceil(checkboxes.length / columnCount);
-
-  const checkboxColumns = Array.from({ length: columnCount }, (_, columnIndex) => (
-    <div key={columnIndex} className="col">
-      {checkboxes.slice(columnIndex * checkboxesPerColumn, (columnIndex + 1) * checkboxesPerColumn).map((checkbox, index) => (
-        <div key={index} className="form-check">
-          <input
-            type="checkbox"
-            className="form-check-input"
-            id={`checkbox-${index}`}
-            value={checkbox}
-            checked={selectedCheckboxes.includes(checkbox)}
-            onChange={handleCheckboxChange}
-          />
-          <label className="form-check-label" htmlFor={`checkbox-${index}`}>
-            {checkbox}
-          </label>
-        </div>
-      ))}
-    </div>
-  ));
-  // Жалобы End ----------------------------------------
 
   return (
     <div className="container">
@@ -82,164 +42,109 @@ function AnalizForm() {
         <hr/>
         <div className="form-container">
           <h3>Наличие жалоб</h3>
-          <div className="parameter-group">
-            <div className="row">
-              {checkboxColumns}
+          <div className="row">
+
+            <div className="col">
+              <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+                <label class="form-check-label" for="flexCheckDefault">
+                  Пониженная температура
+                </label>
+              </div>
+              <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" />
+                <label class="form-check-label" for="flexCheckChecked">
+                  Повышенная температура
+                </label>
+              </div>
+              <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" />
+                <label class="form-check-label" for="flexCheckChecked">
+                  Головокружение
+                </label>
+              </div>
             </div>
+
+            <div className="col">
+              <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+                <label class="form-check-label" for="flexCheckDefault">
+                  Головная боль
+                </label>
+              </div>
+              <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" />
+                <label class="form-check-label" for="flexCheckChecked">
+                  Слабость
+                </label>
+              </div>
+              <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" />
+                <label class="form-check-label" for="flexCheckChecked">
+                  Другое (мы учтем в доработке алгоритма)
+                </label>
+              </div>
+            </div>
+
+            <div className="col">
+              <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+                <label class="form-check-label" for="flexCheckDefault">
+                  Бессоница
+                </label>
+              </div>
+              <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" />
+                <label class="form-check-label" for="flexCheckChecked">
+                  Тошнота
+                </label>
+              </div>
+            </div>
+
           </div>
         </div>
         
         <hr/>
-
         <div className="form-container">
-          <h3>Значения показателей</h3>
-          <div className="parameter-group">
-            <span className="parameter-label">Гематокрит</span>
-            <input id="hematocrit" className="parameter-input" type="number" step="0.1" />
-            <span className="parameter-unit">%</span>
-          </div>
-
-          <div className="parameter-group">
-            <span className="parameter-label">Гемоглобин</span>
-            <input id="hemoglobin" className="parameter-input" type="number" step="0.1" />
-            <span className="parameter-unit">г/дл</span>
-          </div>
-
-          <div className="parameter-group">
-            <span className="parameter-label">Эритроциты</span>
-            <input id="redBloodCells" className="parameter-input" type="number" step="0.1" />
-            <span className="parameter-unit">млн/мкл</span>
-          </div>
-
-          <div className="parameter-group">
-            <span className="parameter-label">MCV (ср. объем эритр.)</span>
-            <input id="mcv" className="parameter-input" type="number" step="0.1" />
-            <span className="parameter-unit">фл</span>
-          </div>
-
-          <div className="parameter-group">
-            <span className="parameter-label">RDW (шир. распред. эритр)</span>
-            <input id="rdw" className="parameter-input" type="number" step="0.1" />
-            <span className="parameter-unit">%</span>
-          </div>
-
-          <div className="parameter-group">
-            <span className="parameter-label">MCH (ср. содер. Hb в эр.)</span>
-            <input id="mch" className="parameter-input" type="number" step="0.1" />
-            <span className="parameter-unit">пг</span>
-          </div>
-
-          <div className="parameter-group">
-            <span className="parameter-label">МСHС (ср. конц. Hb в эр.)</span>
-            <input id="mchc" className="parameter-input" type="number" step="0.1" />
-            <span className="parameter-unit">%</span>
-          </div>
-
-          <div className="parameter-group">
-            <span className="parameter-label">Тромбоциты</span>
-            <input id="platelets" className="parameter-input" type="number" step="0.1" />
-            <span className="parameter-unit">тыс/мкл</span>
-          </div>
-
-          <div className="parameter-group">
-            <span className="parameter-label">Лейкоциты</span>
-            <input id="leukocytes" className="parameter-input" type="number" step="0.1" />
-            <span className="parameter-unit">тыс/мкл</span>
-          </div>
-
-          <div className="parameter-group">
-            <span className="parameter-label">Нейтрофилы (общ.число)</span>
-            <input id="neutrophilsPercent" className="parameter-input" type="number" step="0.1" />
-            <span className="parameter-unit">%</span>
-          </div>
-
-          <div className="parameter-group">
-            <span className="parameter-label">Лимфоциты</span>
-            <input id="lymphocytesPercent" className="parameter-input" type="number" step="0.1" />
-            <span className="parameter-unit">%</span>
-          </div>
-
-          <div className="parameter-group">
-            <span className="parameter-label">Моноциты</span>
-            <input id="monocytesPercent" className="parameter-input" type="number" step="0.1" />
-            <span className="parameter-unit">%</span>
-          </div>
-
-          <div className="parameter-group">
-            <span className="parameter-label">Эозинофилы</span>
-            <input id="eosinophilsPercent" className="parameter-input" type="number" step="0.1" />
-            <span className="parameter-unit">%</span>
-          </div>
-
-          <div className="parameter-group">
-            <span className="parameter-label">Базофилы</span>
-            <input id="basophilsPercent" className="parameter-input" type="number" step="0.1" />
-            <span className="parameter-unit">%</span>
-          </div>
-
-          <div className="parameter-group">
-            <span className="parameter-label">Нейтрофилы, абс.</span>
-            <input id="neutrophilsAbsolute" className="parameter-input" type="number" step="0.1" />
-            <span className="parameter-unit">тыс/мкл</span>
-          </div>
-
-          <div className="parameter-group">
-            <span className="parameter-label">Лимфоциты, абс.</span>
-            <input id="lymphocytesAbsolute" className="parameter-input" type="number" step="0.1" />
-            <span className="parameter-unit">тыс/мкл</span>
-          </div>
-
-          <div className="parameter-group">
-            <span className="parameter-label">Моноциты, абс.</span>
-            <input id="monocytesAbsolute" className="parameter-input" type="number" step="0.1" />
-            <span className="parameter-unit">тыс/мкл</span>
-          </div>
-
-          <div className="parameter-group">
-            <span className="parameter-label">Эозинофилы, абс.</span>
-            <input id="eosinophilsAbsolute" className="parameter-input" type="number" step="0.1" />
-            <span className="parameter-unit">тыс/мкл</span>
-          </div>
-
-          <div className="parameter-group">
-            <span className="parameter-label">Базофилы, абс.</span>
-            <input id="basophilsAbsolute" className="parameter-input" type="number" step="0.1" />
-            <span className="parameter-unit">тыс/мкл</span>
-          </div>
-
-          <div className="parameter-group">
-            <span className="parameter-label">СОЭ</span>
-            <input id="esr" className="parameter-input" type="number" step="0.1" />
-            <span className="parameter-unit">мм/ч</span>
-          </div>
-
+        <h3>Значения показателей</h3>
+          <MetricInput metricName="Гематокрит" unitOfMeasurement="%" idInput="hematocrit" />
+          <MetricInput metricName="Гемоглобин" unitOfMeasurement="г/дл" idInput="hemoglobin" />
+          <MetricInput metricName="Эритроциты" unitOfMeasurement="млн/мкл" idInput="redBloodCells" />
+          <MetricInput metricName="MCV (ср. объем эритр.)" unitOfMeasurement="фл" idInput="mcv" />
+          <MetricInput metricName="RDW (шир. распред. эритр)" unitOfMeasurement="%" idInput="rdw" />
+          <MetricInput metricName="MCH (ср. содер. Hb в эр.)" unitOfMeasurement="пг" idInput="mch" />
+          <MetricInput metricName="МСHС (ср. конц. Hb в эр.)" unitOfMeasurement="%" idInput="mchc" />
+          <MetricInput metricName="Тромбоциты" unitOfMeasurement="тыс/мкл" idInput="platelets" />
+          <MetricInput metricName="Лейкоциты" unitOfMeasurement="тыс/мкл" idInput="leukocytes" />
+          <MetricInput metricName="Нейтрофилы (общ.число)" unitOfMeasurement="%" idInput="neutrophilsPercent" />
+          <MetricInput metricName="Лимфоциты" unitOfMeasurement="%" idInput="lymphocytesPercent" />
+          <MetricInput metricName="Моноциты" unitOfMeasurement="%" idInput="monocytesPercent" />
+          <MetricInput metricName="Эозинофилы" unitOfMeasurement="%" idInput="eosinophilsPercent" />
+          <MetricInput metricName="Базофилы" unitOfMeasurement="%" idInput="basophilsPercent" />
+          <MetricInput metricName="Нейтрофилы, абс." unitOfMeasurement="тыс/мкл" idInput="neutrophilsAbsolute" />
+          <MetricInput metricName="Лимфоциты, абс." unitOfMeasurement="тыс/мкл" idInput="lymphocytesAbsolute" />
+          <MetricInput metricName="Моноциты, абс." unitOfMeasurement="тыс/мкл" idInput="monocytesAbsolute" />
+          <MetricInput metricName="Эозинофилы, абс." unitOfMeasurement="тыс/мкл" idInput="eosinophilsAbsolute" />
+          <MetricInput metricName="Базофилы, абс." unitOfMeasurement="тыс/мкл" idInput="basophilsAbsolute" />
+          <MetricInput metricName="СОЭ" unitOfMeasurement="мм/ч" idInput="esr" />
         </div>
-        <div>
-          <div>
-            <label>
-              <input
-                type="checkbox"
-                checked={isChecked}
-                onChange={handleCheckboxChange}
-              />
+        <br/> 
+        <div class="col-12">
+          <div class="form-check">
+            <input class="form-check-input" type="checkbox" id="gridCheck" checked={isChecked} onChange={handleCheckboxChange} />
+            <label class="form-check-label" for="gridCheck">
               Я согласен с тем, что администрация сайта не несет ответственности за возможные отрицательные последствия, 
               возникшие в результате самостоятельной интерпретации своих анализов и назначения лечения или бездействия. 
               Я понимаю, что результаты расшифровки анализов носят только информационный характер, не являются диагнозом 
               и не заменяют очной консультации врача.
             </label>
           </div>
-          <hr/>
-          <div>
-            <button
-              type="submit"
-              className="btn btn-primary"
-              disabled={!isChecked}
-            >
-              Получить расшифровку показателей
-            </button>
-          </div>
         </div>
-        
+        <br/> 
+        <div class="col-12">
+          <button type="submit" class="btn btn-primary mb-6" disabled={!isChecked}>Получить расшифровку показателей</button>
+        </div>      
+        <br/>  
 
       </Form>
     </div>
---
+++ frontend/src/components/MetricInput.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const MetricInput = ({ metricName, unitOfMeasurement, idInput }) => {
+  return (
+    <div className="row">
+        <div className="col">
+            <div className="row">
+            <div className="col-sm-3">
+                <label className="form-label">{metricName}</label>
+            </div>
+            <div className="col-sm-3 input-group-sm">
+                <input id={idInput} type="number" step="0.1" className="form-control" placeholder="Введите значение" />
+            </div>
+            <div className="col-sm-2">
+                <label className="form-label">{unitOfMeasurement}</label>
+            </div>
+            </div>
+        </div>
+    </div>
+  );
+};
+
+export default MetricInput;
