19d96de2b8476afd6c0c83830fd1d7df889e61d3
Ivan Shamaev <qlik@ivan-shamaev.ru>
2023-08-15 20:15:33 +0000

    have made some improvements: mobile menu, color url field

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,25 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Navbar = () => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
   return (
-    
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div class="container d-flex flex-wrap">
-        <a className="navbar-brand" href="/">Сервис расшифровки общего анализа крови</a>
-        
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
+      <div className="container">
+        <a className="navbar-brand" href="/">
+          {/* <img
+            src="analiz_krovi_blood_logo.png"
+            alt="Логотип"
+            className="logo-img"
+            width="25"
+            height="25"
+          /> */}
+           Расшифровка анализа крови
+        </a>
+
+        <button
+          className="navbar-toggler"
+          type="button"
+          onClick={toggleMobileMenu}
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div
+          className={`collapse navbar-collapse ${
+            isMobileMenuOpen ? 'show' : ''
+          }`}
+        >
+          <ul className="navbar-nav ml-auto">
             <li className="nav-item">
-              <a className="nav-link" href="/">Главная</a>
+              <a className="nav-link" href="/">
+                Главная
+              </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/blog">Блог</a>
+              <a className="nav-link" href="/blog">
+                Блог
+              </a>
             </li>
           </ul>
         </div>
       </div>
     </nav>
-    
   );
 };
 
